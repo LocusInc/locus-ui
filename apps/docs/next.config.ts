46114ec31dd80f43edc19d6d3d97550642d18f88
AsteroidDown <jasper@stahmer.ca>
2026-03-23 22:35:25 -0400

    Transpile for vercel

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@locus-ui/components"],
 };
 
 export default nextConfig;
