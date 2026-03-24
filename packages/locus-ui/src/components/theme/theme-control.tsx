4a8a613b89f8b45e3d53f672ef40eef949214c6c
AsteroidDown <jasper@stahmer.ca>
2026-03-23 21:53:09 -0400

    Updated theme control to show/hide

--- packages/locus-ui/src/components/theme/theme-control.tsx
+++ packages/locus-ui/src/components/theme/theme-control.tsx
@@ -1,9 +1,28 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { Box } from "../box";
+import { Portal } from "../portal";
 import { Select } from "../select";
 import { useTheme } from "./theme-context";
 
-export function ThemeControl() {
+const ThemeControlPosition = [
+  "tl",
+  "top",
+  "tr",
+  "left",
+  "center",
+  "right",
+  "bl",
+  "bottom",
+  "br",
+] as const;
+
+interface ThemeControlProps {
+  position?: (typeof ThemeControlPosition)[number];
+}
+
+export function ThemeControl({ position = "bottom" }: ThemeControlProps) {
   const {
     appearance,
     onAppearanceChange,
@@ -15,81 +34,102 @@ export function ThemeControl() {
     onSpacingChange,
   } = useTheme();
 
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.altKey && event.code === "KeyT") {
+        event.preventDefault();
+        setVisible(!visible);
+      }
+    }
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [visible]);
+
   return (
-    <div className="flex flex-col gap-2">
-      <Select.Root
-        variant="solid"
-        value={appearance}
-        onValueChange={(change) => onAppearanceChange?.(change as any)}
-      >
-        <Select.Label position="inside">Theme Settings</Select.Label>
+    <Portal.Root open={visible} onOpenChange={setVisible}>
+      <Portal.Content position={position}>
+        <Box
+          m="4"
+          p="4"
+          className="flex flex-col gap-2 border-[rgba(var(--border-color), 0.6)]"
+        >
+          <Select.Root
+            variant="solid"
+            value={appearance}
+            onValueChange={(change) => onAppearanceChange?.(change as any)}
+          >
+            <Select.Label position="inside">Theme Settings</Select.Label>
 
-        <Select.Trigger />
+            <Select.Trigger />
 
-        <Select.Content>
-          <Select.Item value="light">Light</Select.Item>
-          <Select.Item value="dark">Dark</Select.Item>
-        </Select.Content>
-      </Select.Root>
+            <Select.Content>
+              <Select.Item value="light">Light</Select.Item>
+              <Select.Item value="dark">Dark</Select.Item>
+            </Select.Content>
+          </Select.Root>
 
-      <Select.Root
-        variant="solid"
-        value={radius}
-        onValueChange={(change) => onRadiusChange?.(change as any)}
-      >
-        <Select.Label position="inside">Radius</Select.Label>
+          <Select.Root
+            variant="solid"
+            value={radius}
+            onValueChange={(change) => onRadiusChange?.(change as any)}
+          >
+            <Select.Label position="inside">Radius</Select.Label>
 
-        <Select.Trigger />
+            <Select.Trigger />
 
-        <Select.Content>
-          <Select.Item value="none">None</Select.Item>
-          <Select.Item value="xs">XS</Select.Item>
-          <Select.Item value="sm">SM</Select.Item>
-          <Select.Item value="md">MD</Select.Item>
-          <Select.Item value="lg">LG</Select.Item>
-          <Select.Item value="xl">XL</Select.Item>
-          <Select.Item value="full">FULL</Select.Item>
-        </Select.Content>
-      </Select.Root>
+            <Select.Content>
+              <Select.Item value="none">None</Select.Item>
+              <Select.Item value="xs">XS</Select.Item>
+              <Select.Item value="sm">SM</Select.Item>
+              <Select.Item value="md">MD</Select.Item>
+              <Select.Item value="lg">LG</Select.Item>
+              <Select.Item value="xl">XL</Select.Item>
+              <Select.Item value="full">FULL</Select.Item>
+            </Select.Content>
+          </Select.Root>
 
-      <Select.Root
-        variant="solid"
-        value={roundness}
-        onValueChange={(change) => onRoundnessChange?.(change as any)}
-      >
-        <Select.Label className="min-w-40!" position="inside">
-          Roundness
-        </Select.Label>
+          <Select.Root
+            variant="solid"
+            value={roundness}
+            onValueChange={(change) => onRoundnessChange?.(change as any)}
+          >
+            <Select.Label className="min-w-40!" position="inside">
+              Roundness
+            </Select.Label>
 
-        <Select.Trigger />
+            <Select.Trigger />
 
-        <Select.Content>
-          <Select.Item value="1">1</Select.Item>
-          <Select.Item value="2">2</Select.Item>
-          <Select.Item value="3">3</Select.Item>
-          <Select.Item value="4">4</Select.Item>
-          <Select.Item value="5">5</Select.Item>
-          <Select.Item value="6">6</Select.Item>
-        </Select.Content>
-      </Select.Root>
+            <Select.Content>
+              <Select.Item value="1">1</Select.Item>
+              <Select.Item value="2">2</Select.Item>
+              <Select.Item value="3">3</Select.Item>
+              <Select.Item value="4">4</Select.Item>
+              <Select.Item value="5">5</Select.Item>
+              <Select.Item value="6">6</Select.Item>
+            </Select.Content>
+          </Select.Root>
 
-      <Select.Root
-        variant="solid"
-        value={spacing}
-        onValueChange={(change) => onSpacingChange?.(change as any)}
-      >
-        <Select.Label position="inside">Spacing</Select.Label>
+          <Select.Root
+            variant="solid"
+            value={spacing}
+            onValueChange={(change) => onSpacingChange?.(change as any)}
+          >
+            <Select.Label position="inside">Spacing</Select.Label>
 
-        <Select.Trigger />
+            <Select.Trigger />
 
-        <Select.Content>
-          <Select.Item value="xs">XS</Select.Item>
-          <Select.Item value="sm">SM</Select.Item>
-          <Select.Item value="md">MD</Select.Item>
-          <Select.Item value="lg">LG</Select.Item>
-          <Select.Item value="xl">XL</Select.Item>
-        </Select.Content>
-      </Select.Root>
-    </div>
+            <Select.Content>
+              <Select.Item value="xs">XS</Select.Item>
+              <Select.Item value="sm">SM</Select.Item>
+              <Select.Item value="md">MD</Select.Item>
+              <Select.Item value="lg">LG</Select.Item>
+              <Select.Item value="xl">XL</Select.Item>
+            </Select.Content>
+          </Select.Root>
+        </Box>
+      </Portal.Content>
+    </Portal.Root>
   );
 }
