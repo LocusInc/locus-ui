b84169654e8c6a841d95f30009a3a5aa4fad1fb6
AsteroidDown <jasper@stahmer.ca>
2026-03-29 18:41:26 -0400

    Explicit component exports for SSR

--- packages/locus-ui/src/index.ts
+++ packages/locus-ui/src/index.ts
@@ -1,1 +1,26 @@
-export * from "./components";
+export {
+  Accordion,
+  Badge,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Flex,
+  Panel,
+  Portal,
+  ProgressBar,
+  Select,
+  Separator,
+  Switch,
+  Text,
+  Theme,
+  ThemeControl,
+  useTheme,
+} from "./components";
+
+export type {
+  ThemeAppearance,
+  ThemeRadius,
+  ThemeRoundness,
+  ThemeSpacing,
+} from "./components";
